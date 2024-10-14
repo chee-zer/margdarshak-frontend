120c3577952bf419fb4a51337b9b01c3235ee94e
neoandmatrix <tusharannand@gmail.com>
2024-10-14 13:16:43 +0530

    feat:Add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return <div className="text-3xl font-bold underline">
     hello world
+    <div>shadcn button</div>
+    <Button>click me</Button>
   </div>
 }
 
