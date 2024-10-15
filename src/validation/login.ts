7c582e978f5416ac2f0e49d7932a3e7c4c1e140b
dhairyawan <dhairyawankumartiwari@gmail.com>
2024-10-16 02:42:38 +0530

    Fixed issue with validation

--- src/validation/login.ts
+++ src/validation/login.ts
@@ -3,7 +3,20 @@ import * as z from "zod"
 export const phoneRegex = /^\+?[1-9]\d{1,14}$/
 
 export const formSchema = z.object({
-  phone: z.string().regex(phoneRegex, "Invalid phone number"),
-  email: z.string().email("Invalid email address"),
+  phone: z.union([z.string().regex(phoneRegex, "Invalid phone number"), z.literal("")]),
+  email: z.union([z.string().email("Invalid email address"), z.literal("")]),
   password: z.string().min(8, "Password must be at least 8 characters"),
-})
+}).superRefine((data, ctx) => {
+  if (data.phone === "" && data.email === "") {
+    ctx.addIssue({
+      code: 'custom',
+      message: "Enter Phone Number or Email",
+      path: ["phone"]
+    });
+    ctx.addIssue({
+      code: 'custom',
+      message: "Enter Phone Number or Email",
+      path: ["email"]
+    });
+  }
+});
